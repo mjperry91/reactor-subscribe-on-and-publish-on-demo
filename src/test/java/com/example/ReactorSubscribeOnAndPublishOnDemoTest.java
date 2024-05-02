fee499a5a812f20feb0915b794f3a51426de9a86
Matt Perry <perrym@objectcomputing.com>
2024-05-02 11:24:43 -0500

    Adding examples with callable

--- src/test/java/com/example/ReactorSubscribeOnAndPublishOnDemoTest.java
+++ src/test/java/com/example/ReactorSubscribeOnAndPublishOnDemoTest.java
@@ -4,6 +4,7 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
@@ -138,6 +139,74 @@ class ReactorSubscribeOnAndPublishOnDemoTest {
     StepVerifier.create(flux)
             .expectSubscription()
             .expectNext(1, 2, 3, 4)
+            .verifyComplete();
+  }
+
+  // Generally, publishOn only affects downstream, but when publishOn's argument
+  // is a callable the callable will be affected as well. This is because reactor
+  // detects this, and does a subscribeOn instead.
+  // See this reactor code for a better understanding:
+  // https://github.com/reactor/reactor-core/blob/d58d344252de96f1b5c47ad18f99fa7066a78f5c/reactor-core/src/main/java/reactor/core/publisher/Mono.java#L3243
+  @Test
+  void publishOn_after_callable(){
+
+    Mono<String> mono = Mono.fromCallable(() -> {
+      System.out.println("Callable is on Thread " + Thread.currentThread().getName());
+      return "done";
+    })
+    .publishOn(Schedulers.boundedElastic())
+    .map(s -> {
+      System.out.println("Map is on Thread " + Thread.currentThread().getName());
+      return s;
+    });
+
+    StepVerifier.create(mono)
+            .expectSubscription()
+            .expectNext("done")
+            .verifyComplete();
+  }
+
+  // To prevent the callable from being affected by the downstream publishOn you can use
+  // the hide() method. As you will see the callable will be executed on the initial
+  // calling test thread instead.
+  @Test
+  void publishOn_after_callable_with_hide(){
+
+    Mono<String> mono = Mono.fromCallable(() -> {
+              System.out.println("Callable is on Thread " + Thread.currentThread().getName());
+              return "done";
+            })
+            .hide()
+            .publishOn(Schedulers.boundedElastic())
+            .map(s -> {
+              System.out.println("Map is on Thread " + Thread.currentThread().getName());
+              return s;
+            });
+
+    StepVerifier.create(mono)
+            .expectSubscription()
+            .expectNext("done")
+            .verifyComplete();
+  }
+
+  // SubscribeOn does as expected and affects the execution thread both upstream and downstream,
+  // and hide() will have no effect.
+  @Test
+  void subscribeOn_after_callable(){
+
+    Mono<String> mono = Mono.fromCallable(() -> {
+              System.out.println("Callable is on Thread " + Thread.currentThread().getName());
+              return "done";
+            })
+            .subscribeOn(Schedulers.boundedElastic())
+            .map(s -> {
+              System.out.println("Map is on Thread " + Thread.currentThread().getName());
+              return s;
+            });
+
+    StepVerifier.create(mono)
+            .expectSubscription()
+            .expectNext("done")
             .verifyComplete();
   }
 }
